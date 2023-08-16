d6869e9302cb3779edd7e34caa02000ec651befb
Adler Araújo <adlaraujo@deloitte.pt>
2023-08-16 14:56:31 +0100

    todo-list code

---
+++ script.js
@@ -0,0 +1,13 @@
+function addTask() {
+    const taskInput = document.getElementById("taskInput");
+    const taskText = taskInput.value.trim();
+    if(taskText) {
+        const li = document.createElement("li");
+        li.textContent = taskText;
+        li.onclick = function() {
+            this.remove();
+        };
+        document.getElementById("taskList").appendChild(li);
+        taskInput.value = "";
+    }
+}
